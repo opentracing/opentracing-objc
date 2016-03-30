7a9cd5c0bc6b3861fc3f2321ad24a490edc7dd54
Ben Sigelman <bhs@resonancelabs.com>
2016-03-29 22:16:48 -0700

    Increment version to 0.1.1

--- Pod/Classes/OTNoop.m
+++ Pod/Classes/OTNoop.m
@@ -1,40 +1,60 @@
-@class OTNoopTracer : NSObject<OTTracer>
+#import "OTNoop.h"
 
-- (id<OTSpan>)startSpan:(NSString*)operationName;
+@implementation OTNoopTracer
+
+- (id<OTSpan>)startSpan:(NSString*)operationName {
+    return nil;
+}
 
 - (id<OTSpan>)startSpan:(NSString*)operationName
-		   tags:(NSDictionary*)tags;
+		   tags:(NSDictionary*)tags {
+    return nil;
+}
 
 - (id<OTSpan>)startSpan:(NSString*)operationName
-		 parent:(id<OTSpan>)parentSpan;
+		 parent:(id<OTSpan>)parentSpan {
+    return nil;
+}
 
 - (id<OTSpan>)startSpan:(NSString*)operationName
 		 parent:(id<OTSpan>)parentSpan
-		   tags:(NSDictionary*)tags;
+		   tags:(NSDictionary*)tags {
+    return nil;
+}
 
 - (id<OTSpan>)startSpan:(NSString*)operationName
 		 parent:(id<OTSpan>)parentSpan
 		   tags:(NSDictionary*)tags
-	      startTime:(NSDate*)startTime;
+	      startTime:(NSDate*)startTime {
+    return nil;
+}
 
 
 - (bool)inject:(id<OTSpan>)span
        	format:(NSString*)format
-       carrier:(id)carrier;
+       carrier:(id)carrier {
+    return false;
+}
 
 - (bool)inject:(id<OTSpan>)span
        	format:(NSString*)format
        carrier:(id)carrier
-	 error:(NSError* __autoreleasing *)outError;
+	 error:(NSError* __autoreleasing *)outError {
+    return false;
+}
 
 
 - (id<OTSpan>)join:(NSString*)operationName
 	    format:(NSString*)format
-	   carrier:(id)carrier;
+	   carrier:(id)carrier {
+    return nil;
+}
 
 - (id<OTSpan>)join:(NSString*)operationName
 	    format:(NSString*)format
 	   carrier:(id)carrier
-	     error:(NSError* __autoreleasing *)outError;
+	     error:(NSError* __autoreleasing *)outError {
+    return nil;
+}
 
 @end
--- Pod/Classes/OTVersion.h
+++ Pod/Classes/OTVersion.h
@@ -1,3 +1,3 @@
 // GENERATED FILE: Do not edit directly
-#define OT_TRACER_VERSION @"0.1.0"
+#define OT_TRACER_VERSION @"0.1.1"
 
