beb2a5ee71715162f80750019372534cc5413c5e
Ben Sigelman <bhs@lightstep.com>
2016-07-22 09:49:35 -0700

    First pass at SpanContext changes

--- Pod/Classes/OTNoop.m
+++ Pod/Classes/OTNoop.m
@@ -1,45 +1,57 @@
 #import "OTNoop.h"
 #import "OTSpan.h"
+#import "OTSpanContext.h"
 
 @class OTNoopSpan;
+@class OTNoopSpanContext;
 
 // Initialized in OTNoopTracer.initialize:
 static OTNoopTracer* g_defaultNoopTracer;
 static OTNoopSpan* g_defaultNoopSpan;
+static OTNoopSpanContext* g_defaultNoopSpanContext;
+
+@interface OTNoopSpanContext : NSObject<OTSpanContext>
+@end
+
+@implementation OTNoopSpanContext
+- (void)setBaggageItem:(NSString*)key value:(NSString*)value {}
+- (NSString*)getBaggageItem:(NSString*)key { return nil; }
+@end
 
 @interface OTNoopSpan : NSObject<OTSpan>
 @end
 
 @implementation OTNoopSpan
 - (id<OTTracer>)tracer { return g_defaultNoopTracer; }
+- (id<OTSpanContext>)context { return g_defaultNoopSpanContext; }
 - (void)setOperationName:(NSString*)operationName {}
 - (void)setTag:(NSString*)key value:(NSString*)value {}
 - (void)logEvent:(NSString*)eventName {}
 - (void)logEvent:(NSString*)eventName payload:(NSObject*)payload {}
 - (void)log:(NSString*)eventName timestamp:(NSDate*)timestamp payload:(NSObject*)payload {}
-- (void)setBaggageItem:(NSString*)key value:(NSString*)value {}
-- (NSString*)getBaggageItem:(NSString*)key { return nil; }
-- (void) finish {}
-- (void) finishWithTime:(NSDate*)finishTime {}
+- (void)finish {}
+- (void)finishWithTime:(NSDate*)finishTime {}
 @end
 
 @implementation OTNoopTracer
 
 + (void)initialize {
     g_defaultNoopSpan = [OTNoopSpan new];
+    g_defaultNoopSpanContext = [OTNoopSpanContext new];
     g_defaultNoopTracer = [OTNoopTracer new];
 }
 
 - (id<OTSpan>)startSpan:(NSString*)operationName { return g_defaultNoopSpan; }
 - (id<OTSpan>)startSpan:(NSString*)operationName tags:(NSDictionary*)tags { return g_defaultNoopSpan; }
-- (id<OTSpan>)startSpan:(NSString*)operationName parent:(id<OTSpan>)parentSpan { return g_defaultNoopSpan; }
-- (id<OTSpan>)startSpan:(NSString*)operationName parent:(id<OTSpan>)parentSpan tags:(NSDictionary*)tags { return g_defaultNoopSpan; }
-- (id<OTSpan>)startSpan:(NSString*)operationName parent:(id<OTSpan>)parentSpan tags:(NSDictionary*)tags startTime:(NSDate*)startTime { return g_defaultNoopSpan; }
+- (id<OTSpan>)startSpan:(NSString*)operationName childOf:(id<OTSpanContext>)parent { return g_defaultNoopSpan; }
+- (id<OTSpan>)startSpan:(NSString*)operationName childOf:(id<OTSpanContext>)parent tags:(NSDictionary*)tags { return g_defaultNoopSpan; }
+- (id<OTSpan>)startSpan:(NSString*)operationName childOf:(id<OTSpanContext>)parent tags:(NSDictionary*)tags startTime:(NSDate*)startTime { return g_defaultNoopSpan; }
+- (id<OTSpan>)startSpan:(NSString*)operationName references:(NSArray*)references tags:(NSDictionary*)tags startTime:(NSDate*)startTime { return g_defaultNoopSpan; }
 
-- (bool)inject:(id<OTSpan>)span format:(NSString*)format carrier:(id)carrier { return true; }
-- (bool)inject:(id<OTSpan>)span format:(NSString*)format carrier:(id)carrier error:(NSError* __autoreleasing *)outError { return true; }
+- (bool)inject:(id<OTSpanContext>)span format:(NSString*)format carrier:(id)carrier { return true; }
+- (bool)inject:(id<OTSpanContext>)span format:(NSString*)format carrier:(id)carrier error:(NSError* __autoreleasing *)outError { return true; }
 
-- (id<OTSpan>)join:(NSString*)operationName format:(NSString*)format carrier:(id)carrier { return nil; }
-- (id<OTSpan>)join:(NSString*)operationName format:(NSString*)format carrier:(id)carrier error:(NSError* __autoreleasing *)outError { return nil; }
+- (id<OTSpanContext>)extract:(NSString*)format carrier:(id)carrier { return nil; }
+- (id<OTSpanContext>)extract:(NSString*)format carrier:(id)carrier error:(NSError* __autoreleasing *)outError { return nil; }
 
 @end
