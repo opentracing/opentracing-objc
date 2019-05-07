dfab031fefb89b83f7b00bb870b06f1bf7a35f63
Ignacio Bonafonte <nacho@undefinedlabs.com>
2019-05-07 12:15:22 +0200

    Add project file with the framework as only target

---
+++ OpenTracing Framework/opentracing.h
@@ -0,0 +1,26 @@
+//
+//  OpenTracing.h
+//  OpenTracing
+//
+//  Created by Ignacio Bonafonte on 07/05/2019.
+//  Copyright © 2019 OpenTracing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenTracing.
+FOUNDATION_EXPORT double OpenTracingVersionNumber;
+
+//! Project version string for OpenTracing.
+FOUNDATION_EXPORT const unsigned char OpenTracingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenTracing/PublicHeader.h>
+
+#import <OpenTracing/OTTracer.h>
+#import <OpenTracing/OTSpanContext.h>
+#import <OpenTracing/OTSpan.h>
+#import <OpenTracing/OTNoop.h>
+#import <OpenTracing/OTGlobal.h>
+#import <OpenTracing/OTReference.h>
+#import <OpenTracing/OTVersion.h>
+
